be7270ac5cb4f5405239b1f457afa0dc6bca1378
Akil1928 <akilwatsonch1928@gmail.com>
2025-06-02 20:24:54 -0600

    First create

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ucr.lab.proyectogrupo5 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ucr.lab.proyectogrupo5 to javafx.fxml;
+    exports ucr.lab.proyectogrupo5;
+}
